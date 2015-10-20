f20bed68037e47db77adf20221e942f073694210
XueSeason <496867094@qq.com>
2015-10-20 08:47:19 +0800

    fixed XSShoppingCartViewController bug

--- SanjiangShopping/SanjiangShopping/Controller/购物车/XSShoppingCartViewController.m
+++ SanjiangShopping/SanjiangShopping/Controller/购物车/XSShoppingCartViewController.m
@@ -36,7 +36,7 @@ static NSString * const addressID = @"Address";
 @property (weak, nonatomic) IBOutlet UILabel *discountLabel;
 @property (weak, nonatomic) IBOutlet UIButton *settlementButton;
 
-@property (strong, nonatomic) IBOutlet UIView *editPanelView;
+@property (strong, nonatomic)  UIView *editPanelView;
 @property (weak, nonatomic) IBOutlet UIButton *addToFavoritesButton;
 @property (weak, nonatomic) IBOutlet UIButton *deleteButton;
 
@@ -46,16 +46,11 @@ static NSString * const addressID = @"Address";
 
 @implementation XSShoppingCartViewController
 
+#pragma mark - life cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.navigationItem.title = @"购物车";
-    [XSNavigationBarHelper hackStandardNavigationBar:self.navigationController.navigationBar];
-    
-    UIBarButtonItem *editButton = [[UIBarButtonItem alloc] initWithTitle:@"编辑" style:UIBarButtonItemStylePlain target:self action:@selector(edit:)];
-    editButton.tintColor = [UIColor grayColor];
-    self.navigationItem.rightBarButtonItem = editButton;
-    
+    [self customNavigationBar];
     [self.view addSubview:self.editPanelView];
     [self.view addSubview:self.controlPannelView];
     [self.view addSubview:self.tableView];
@@ -72,12 +67,12 @@ static NSString * const addressID = @"Address";
     
     CGRect pannelFrame;
     if (!self.tabBarController.tabBar.hidden) {
-        pannelFrame = CGRectMake(0, [UIScreen mainScreen].bounds.size.height - self.tabBarController.tabBar.frame.size.height - 49, [UIScreen mainScreen].bounds.size.width, 49);
+        pannelFrame = CGRectMake(0, CGRectGetHeight([UIScreen mainScreen].bounds) - self.tabBarController.tabBar.frame.size.height - 49, CGRectGetWidth([UIScreen mainScreen].bounds), 49);
     } else {
-        pannelFrame = CGRectMake(0, [UIScreen mainScreen].bounds.size.height - 49, [UIScreen mainScreen].bounds.size.width, 49);
+        pannelFrame = CGRectMake(0, CGRectGetHeight([UIScreen mainScreen].bounds) - 49, CGRectGetWidth([UIScreen mainScreen].bounds), 49);
     }
     self.controlPannelView.frame = pannelFrame;
-    self.editPanelView.frame = pannelFrame;
+    self.editPanelView.frame     = pannelFrame;
     
     self.tableView.frame = CGRectMake(0, 0, pannelFrame.size.width, pannelFrame.origin.y);
     
@@ -90,54 +85,6 @@ static NSString * const addressID = @"Address";
         self.view = nil;
     }
 }
-
-#pragma mark - getters and setters 
-- (UIView *)editPanelView {
-    if (_editPanelView == nil) {
-        _editPanelView = [[[NSBundle mainBundle] loadNibNamed:@"ControlPannel" owner:self options:nil] objectAtIndex:1];
-        _editPanelView.hidden = YES;
-        _editPanelView.layer.borderWidth = 0.5f;
-        _editPanelView.layer.borderColor = [[UIColor lightGrayColor] CGColor];
-        
-        [_addToFavoritesButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
-        _addToFavoritesButton.layer.borderWidth  = 1.0f;
-        _addToFavoritesButton.layer.borderColor  = [[UIColor lightGrayColor] CGColor];
-        _addToFavoritesButton.layer.cornerRadius = 5.0f;
-        
-        _deleteButton.layer.cornerRadius = 5.0f;
-    }
-    return _editPanelView;
-}
-
-- (UIView *)controlPannelView {
-    if (_controlPannelView == nil) {
-        _controlPannelView = [[[NSBundle mainBundle] loadNibNamed:@"ControlPannel" owner:self options:nil] objectAtIndex:0];
-        _settlementButton.backgroundColor = THEME_RED;
-        _controlPannelView.layer.borderWidth = 0.5f;
-        _controlPannelView.layer.borderColor = [[UIColor lightGrayColor] CGColor];
-    }
-    return _controlPannelView;
-}
-
-- (UITableView *)tableView {
-    if (_tableView == nil) {
-        _tableView = [[UITableView alloc] init];
-        _tableView.backgroundColor = BACKGROUND_COLOR;
-        _tableView.delegate        = self;
-        _tableView.dataSource      = self;
-        _tableView.contentInset    = UIEdgeInsetsMake(64, 0, 0, 0);
-        _tableView.tableFooterView = [[UIView alloc] init];
-        _tableView.showsHorizontalScrollIndicator = NO;
-        _tableView.showsVerticalScrollIndicator   = NO;
-        
-        [_tableView registerClass:[XSAddressTableViewCell class] forCellReuseIdentifier:addressID];
-        [_tableView registerNib:[UINib nibWithNibName:@"XSAddressTableViewCell" bundle:nil] forCellReuseIdentifier:addressID];
-        [_tableView registerClass:[XSShoppingCartTableViewCell class] forCellReuseIdentifier:cellID];
-        [_tableView registerNib:[UINib nibWithNibName:@"XSShoppingCartTableViewCell" bundle:nil] forCellReuseIdentifier:cellID];
-    }
-    return _tableView;
-}
-
 
 #pragma mark - UITableViewDataSource
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
@@ -214,6 +161,16 @@ static NSString * const addressID = @"Address";
 }
 
 #pragma mark - private methods
+- (void)customNavigationBar {
+    self.navigationItem.title = @"购物车";
+    [XSNavigationBarHelper hackStandardNavigationBar:self.navigationController.navigationBar];
+    
+    UIBarButtonItem *editButton = [[UIBarButtonItem alloc] initWithTitle:@"编辑" style:UIBarButtonItemStylePlain target:self action:@selector(edit:)];
+    editButton.tintColor = [UIColor grayColor];
+    self.navigationItem.rightBarButtonItem = editButton;
+
+}
+
 - (void)downLoadJSONData {
     NSString *urlStr = [NSString stringWithFormat:@"%@%@:%@%@", PROTOCOL, SERVICE_ADDRESS, DEFAULT_PORT, ROUTER_CART_LIST];
     
@@ -257,13 +214,60 @@ static NSString * const addressID = @"Address";
 - (void)edit:(UIBarButtonItem *)sender {
     if ([sender.title isEqualToString:@"编辑"]) {
         sender.title = @"完成";
-        _controlPannelView.hidden = YES;
-        _editPanelView.hidden = NO;
+        self.controlPannelView.hidden = YES;
+        self.editPanelView.hidden = NO;
     } else {
         sender.title = @"编辑";
-        _controlPannelView.hidden = NO;
-        _editPanelView.hidden = YES;
+        self.controlPannelView.hidden = NO;
+        self.editPanelView.hidden = YES;
     }
+}
+
+#pragma mark - getters and setters
+- (UIView *)editPanelView {
+    if (_editPanelView == nil) {
+        _editPanelView = [[[NSBundle mainBundle] loadNibNamed:@"ControlPannel" owner:self options:nil] objectAtIndex:1];
+        _editPanelView.hidden = YES;
+        _editPanelView.layer.borderWidth = 0.5f;
+        _editPanelView.layer.borderColor = [[UIColor lightGrayColor] CGColor];
+        
+        [_addToFavoritesButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+        _addToFavoritesButton.layer.borderWidth  = 1.0f;
+        _addToFavoritesButton.layer.borderColor  = [[UIColor lightGrayColor] CGColor];
+        _addToFavoritesButton.layer.cornerRadius = 5.0f;
+        
+        _deleteButton.layer.cornerRadius = 5.0f;
+    }
+    return _editPanelView;
+}
+
+- (UIView *)controlPannelView {
+    if (_controlPannelView == nil) {
+        _controlPannelView = [[[NSBundle mainBundle] loadNibNamed:@"ControlPannel" owner:self options:nil] objectAtIndex:0];
+        _settlementButton.backgroundColor = THEME_RED;
+        _controlPannelView.layer.borderWidth = 0.5f;
+        _controlPannelView.layer.borderColor = [[UIColor lightGrayColor] CGColor];
+    }
+    return _controlPannelView;
+}
+
+- (UITableView *)tableView {
+    if (_tableView == nil) {
+        _tableView = [[UITableView alloc] init];
+        _tableView.backgroundColor = BACKGROUND_COLOR;
+        _tableView.delegate        = self;
+        _tableView.dataSource      = self;
+        _tableView.contentInset    = UIEdgeInsetsMake(64, 0, 0, 0);
+        _tableView.tableFooterView = [[UIView alloc] init];
+        _tableView.showsHorizontalScrollIndicator = NO;
+        _tableView.showsVerticalScrollIndicator   = NO;
+        
+        [_tableView registerClass:[XSAddressTableViewCell class] forCellReuseIdentifier:addressID];
+        [_tableView registerNib:[UINib nibWithNibName:@"XSAddressTableViewCell" bundle:nil] forCellReuseIdentifier:addressID];
+        [_tableView registerClass:[XSShoppingCartTableViewCell class] forCellReuseIdentifier:cellID];
+        [_tableView registerNib:[UINib nibWithNibName:@"XSShoppingCartTableViewCell" bundle:nil] forCellReuseIdentifier:cellID];
+    }
+    return _tableView;
 }
 
 @end
