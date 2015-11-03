520186b483c8799f3a23809af21e385743e33396
XueSeason <496867094@qq.com>
2015-11-03 16:07:57 +0800

    XSAddressManageTableView

---
+++ SanjiangShopping/SanjiangShopping/Controller/我的三江/我的收货地址/XSAddressManageTableViewCell.m
@@ -0,0 +1,32 @@
+//
+//  XSAddressManageTableViewCell.m
+//  SanjiangShopping
+//
+//  Created by 薛纪杰 on 11/3/15.
+//  Copyright © 2015 Sanjiang Shopping Club Co., Ltd. All rights reserved.
+//
+
+#import "XSAddressManageTableViewCell.h"
+
+#import "ThemeColor.h"
+@interface XSAddressManageTableViewCell()
+@property (weak, nonatomic) IBOutlet UIView *myView;
+
+@end
+@implementation XSAddressManageTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+    self.selectionStyle = UITableViewCellSelectionStyleNone;
+    self.contentView.backgroundColor = BACKGROUND_COLOR;
+    self.myView.layer.borderColor = [[UIColor lightGrayColor] CGColor];
+    self.myView.layer.borderWidth = 0.5;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
--- SanjiangShopping/SanjiangShopping/Controller/我的三江/我的收货地址/XSAddressManageViewController.m
+++ SanjiangShopping/SanjiangShopping/Controller/我的三江/我的收货地址/XSAddressManageViewController.m
@@ -8,16 +8,100 @@
 
 #import "XSAddressManageViewController.h"
 
-@interface XSAddressManageViewController ()
+#import "XSAddressManageTableViewCell.h"
+
+#import "XSNavigationBarHelper.h"
+#import "ThemeColor.h"
+
+static NSString * const cellID = @"address";
+
+@interface XSAddressManageViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (strong, nonatomic) UITableView *tableView;
+@property (strong, nonatomic) UIButton *addButton;
 @end
 
 @implementation XSAddressManageViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self customNavigationBar];
+    [self.view addSubview:self.tableView];
+    [self.view addSubview:self.addButton];
+}
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault];
+    self.navigationController.navigationBarHidden = NO;
+    self.tabBarController.tabBar.hidden           = YES;
     
+    self.tableView.frame = self.view.bounds;
+    self.addButton.frame = CGRectMake(0, CGRectGetHeight(self.view.bounds) - 44, CGRectGetWidth(self.view.bounds), 44);
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 15;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    XSAddressManageTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID forIndexPath:indexPath];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 115.0;
+}
+
+#pragma mark - private methods
+- (void)customNavigationBar {
+    self.navigationItem.title = @"地址管理";
+    [XSNavigationBarHelper hackStandardNavigationBar:self.navigationController.navigationBar];
+    UIBarButtonItem *leftButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"arrow_left"] style:UIBarButtonItemStylePlain target:self action:@selector(comeBack)];
+    leftButtonItem.tintColor = MAIN_TITLE_COLOR;
+    self.navigationItem.leftBarButtonItem = leftButtonItem;
+    self.navigationController.interactivePopGestureRecognizer.delegate = (id<UIGestureRecognizerDelegate>)self;
+}
+
+- (void)comeBack {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)addAddress {
+    NSLog(@"添加地址");
+}
+
+#pragma mark - getters and setters
+- (UITableView *)tableView {
+    if (_tableView == nil) {
+        _tableView = [[UITableView alloc] init];
+        _tableView.tableFooterView = [UIView new];
+        _tableView.backgroundColor = BACKGROUND_COLOR;
+        _tableView.separatorColor  = [UIColor clearColor];
+        
+        _tableView.delegate   = self;
+        _tableView.dataSource = self;
+        
+        _tableView.showsHorizontalScrollIndicator = NO;
+        _tableView.showsVerticalScrollIndicator   = NO;
+        
+        [_tableView registerClass:[XSAddressManageTableViewCell class] forCellReuseIdentifier:cellID];
+        [_tableView registerNib:[UINib nibWithNibName:@"XSAddressManageTableViewCell" bundle:nil] forCellReuseIdentifier:cellID];
+    }
+    return _tableView;
+}
+
+- (UIButton *)addButton {
+    if (_addButton == nil) {
+        _addButton = [[UIButton alloc] init];
+        [_addButton setTitle:@"+ 添加新地址" forState:UIControlStateNormal];
+        _addButton.backgroundColor = [UIColor redColor];
+        _addButton.tintColor = [UIColor whiteColor];
+        [_addButton addTarget:self action:@selector(addAddress) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _addButton;
 }
 
 @end
